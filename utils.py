f51d8837a70a70187e89087752617a40824ed5fa
wx <wx@travelsky.com>
2017-09-25 21:05:09 +0800

    Add func is_ip and checksum to utils.py

--- utils.py
+++ utils.py
@@ -7,8 +7,8 @@
 # # # # # # # # # # # # # # # # # # # # # # 
 
 import re
-import socket
 
+# using regex to find if the string is ip
 regex_ip = r'(((2[0-4]\d|25[0-5]|[01]?\d\d?)\.){3}(2[0-4]\d|25[0-5]|[01]?\d\d?))'
 pattern_ip = re.compile(regex_ip)
 def is_ip(expression):
@@ -17,3 +17,23 @@ def is_ip(expression):
         return True
     else:
         return False
+
+# calculate checksum of a ICMP Packet
+# (may be ok for other packet)
+def checksum(packet):
+    length = len(packet)
+    cks    = 0
+    mod    = length % 2
+
+    for i in range(0,length - mod,2):
+        cks += (packet[i+1] << 8) + packet[i]
+
+    if mod:
+        cks += packet[-1]
+
+    while cks >> 16:
+        cks = (cks & 0xFFFF) + (cks >> 16)
+    cks = ~cks & 0xFFFF
+    cks = cks >> 8 | (cks << 8 & 0xFF00)
+
+    return cks
